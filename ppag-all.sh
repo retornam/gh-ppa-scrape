43b45cdfdb0e796fc97b39c13c2b3dac08e9e3ca
retornam <retornam@users.noreply.github.com>
2023-10-02 23:55:07 -0700

    Speed Improvements
    ./ppag-all.sh  180.40s user 119.71s system 10% cpu 49:00.07 total
    
    ./ppag-all.sh  264.20s user 261.24s system 113% cpu 7:42.67 total
    
    switch to using GNU parallel and rewrite ./ppag-all.sh to be faster

--- ppag-all.sh
+++ ppag-all.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-LAST=$(curl https://tenders.ppa.gov.gh/contracts\?page\=1 | htmlq --text '.page-item:nth-last-child(2)>a.page-link')
+set -euo pipefail
 
-TENDERS=()
-for page in $(seq 1 ${LAST}); do
-    # Make a curl request to the URL
-    echo $page
-    output=$(curl --silent "https://tenders.ppa.gov.gh/contracts?page=$page" |
-        htmlq 'div.list-title > a[href]' --attribute href)
-    TENDERS+=($output)
-done
+fetch_tender_data() {
+    page="${1}"
+    output=$(curl --silent "https://tenders.ppa.gov.gh/contracts?page=$page" |\
+              htmlq 'div.list-title > a[href]' --attribute href)
+    echo "${output}"
+}
 
-##echo "${TENDERS[@]}"
+export -f fetch_tender_data
+LAST=$(curl https://tenders.ppa.gov.gh/contracts?page=1 |\
+        htmlq --text '.page-item:nth-last-child(2)>a.page-link')
+echo "LAST PAGE: ${LAST}"
+TENDERS=("$(seq 1 "${LAST}" | parallel -k -j+0 fetch_tender_data {})")
+# version sort tenders
+SORTED_TENDERS=("$(echo "${TENDERS[@]}" | tr ' ' '\n' | sort --version-sort)")
+# download in parallel
+parallel -k -j+0 \
+'curl --create-dirs --output-dir alltenders -LOJ {}' ::: "${SORTED_TENDERS[@]}"
 
-for item in "${TENDERS[@]}"; do
-    curl --create-dirs  --output-dir alltenders -LOJ $item
-done
