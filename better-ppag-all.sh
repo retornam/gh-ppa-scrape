a1f343bcdd9fdbdb25365b3c0bdb9eee745efee6
retornam <retornam@users.noreply.github.com>
2024-05-24 08:18:22 -0700

    latest updates

--- better-ppag-all.sh
+++ better-ppag-all.sh
@@ -2,18 +2,22 @@
 
 set -euo pipefail
 
+UA="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+
 fetch_tender_data() {
     page="${1}"
-    output=$(curl --silent "https://tenders.ppa.gov.gh/contracts?page=$page" | \
+    output=$(curl -A "${UA}" --silent "https://tenders.ppa.gov.gh/contracts?page=$page" | \
         htmlq 'div.list-title > a[href]' --attribute href)
     echo "${output}"
 }
 
 export -f fetch_tender_data
-LAST=$(curl https://tenders.ppa.gov.gh/contracts?page=1 | \
+
+LAST=$(curl -A "${UA}" https://tenders.ppa.gov.gh/contracts?page=1 | \
     htmlq --text '.page-item:nth-last-child(2)>a.page-link')
 echo "LAST PAGE: ${LAST}"
 TENDERS=($(seq 1 "${LAST}" | parallel -k -j+0 fetch_tender_data {}))
+
 # version sort tenders
 SORTED_TENDERS=($(echo "${TENDERS[@]}" | tr ' ' '\n' | sort --version-sort))
 
@@ -22,8 +26,10 @@ download_file() {
     url="$1"
     curl --create-dirs --output-dir alltenders -LOJ "$url"
 }
+
 export -f download_file
+
 # Use xargs to download the files in batches
-echo "${SORTED_TENDERS[@]}" | xargs -I {} -P 50 -n 1 bash -c 'download_file "$@"' _ {}
+echo "${SORTED_TENDERS[@]}" | xargs -I {} -P 100 -n 1 bash -c 'download_file "$@"' _ {}
 
 
